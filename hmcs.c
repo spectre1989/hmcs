e00113cffb9cf09c9e30bab68ae7bf968e957825
Joe Best-Rotheray <joebestrotheray@gmail.com>
2026-04-12 22:49:05 +0100

    getting swapchain set up

--- hmcs.c
+++ hmcs.c
@@ -139,8 +139,28 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL vk_debug_callback(
 	return VK_FALSE; // don't abort the call
 }
 
+enum {WINDOW_WIDTH = 1280, WINDOW_HEIGHT = 800};
+
+LRESULT CALLBACK window_proc(HWND window_handle, UINT msg, WPARAM w_param, LPARAM l_param)
+{
+	return DefWindowProc(window_handle, msg, w_param, l_param);
+}
+
 int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance, PWSTR cmd_line, int cmd_show)
 {
+	WNDCLASS window_class = {
+		.style = CS_HREDRAW | CS_VREDRAW,
+		.lpfnWndProc = window_proc,
+		.hInstance = instance,
+		.hCursor = LoadCursor(NULL, IDC_ARROW),
+		.lpszClassName = L"hmcs"
+	};
+	RegisterClass(&window_class);
+
+	HWND window_handle = CreateWindow(L"hmcs", L"hmcs", WS_OVERLAPPEDWINDOW, 200, 100, WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL, instance, NULL);
+	assert(window_handle);
+	ShowWindow(window_handle, SW_SHOW);
+
 	g_vk_debug_buffer = malloc(VK_DEBUG_BUFFER_SIZE);
 
 	VkResult result = volkInitialize();
@@ -281,13 +301,14 @@ int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance, PWSTR cmd_line,
 	}
 
 	VkDevice vk_device;
+	uint32_t queue_family_index_count;
+	uint32_t queue_family_indices[2] = { graphics_queue_index, transfer_queue_index };
 	{
-		uint32_t queue_create_info_count;
 		float32_t priority = 1.0f;
 		VkDeviceQueueCreateInfo* queue_create_info;
 		if (graphics_queue_index == transfer_queue_index)
 		{
-			queue_create_info_count = 1;
+			queue_family_index_count = 1;
 			queue_create_info = malloc(sizeof(VkDeviceQueueCreateInfo));
 			queue_create_info[0] = (VkDeviceQueueCreateInfo){
 				.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
@@ -299,8 +320,8 @@ int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance, PWSTR cmd_line,
 		}
 		else
 		{
-			queue_create_info_count = 2;
-			queue_create_info = malloc(sizeof(VkDeviceQueueCreateInfo) * queue_create_info_count);
+			queue_family_index_count = 2;
+			queue_create_info = malloc(sizeof(VkDeviceQueueCreateInfo) * queue_family_index_count);
 			queue_create_info[0] = (VkDeviceQueueCreateInfo){
 				.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
 				.pNext = NULL,
@@ -317,22 +338,232 @@ int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance, PWSTR cmd_line,
 			};
 		}
 
+		const char* extensions[] = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+
 		VkDeviceCreateInfo create_info = {
 			.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
 			.pNext = NULL,
 			.flags = 0,
-			.queueCreateInfoCount = queue_create_info_count,
+			.queueCreateInfoCount = queue_family_index_count,
 			.pQueueCreateInfos = queue_create_info,
 			.enabledLayerCount = 0,
 			.ppEnabledLayerNames = NULL,
-			.enabledExtensionCount = 0,
-			.ppEnabledExtensionNames = NULL,
+			.enabledExtensionCount = sizeof(extensions) / sizeof(extensions[0]),
+			.ppEnabledExtensionNames = extensions,
 			.pEnabledFeatures = NULL
 		};
 		result = vkCreateDevice(vk_physical_device, &create_info, NULL, &vk_device);
 		assert(result == VK_SUCCESS);
 	}
 	volkLoadDevice(vk_device);
+
+	VkSurfaceKHR vk_surface;
+	{
+		VkWin32SurfaceCreateInfoKHR create_info = {
+			.sType = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR,
+			.flags = 0,
+			.hinstance = instance,
+			.hwnd = window_handle
+		};
+		result = vkCreateWin32SurfaceKHR(vk_instance, &create_info, NULL, &vk_surface);
+		assert(result == VK_SUCCESS);
+
+		VkBool32 supported;
+		result = vkGetPhysicalDeviceSurfaceSupportKHR(vk_physical_device, graphics_queue_index, vk_surface, &supported);
+		assert(result == VK_SUCCESS);
+		assert(supported);
+	}
+
+	VkSwapchainKHR vk_swapchain;
+	VkSurfaceCapabilitiesKHR surface_capabilities;
+	VkSurfaceFormatKHR surface_format = { 0 };
+	{
+		result = vkGetPhysicalDeviceSurfaceCapabilitiesKHR(vk_physical_device, vk_surface, &surface_capabilities);
+		assert(result == VK_SUCCESS);
+
+		uint32_t count;
+		result = vkGetPhysicalDeviceSurfaceFormatsKHR(vk_physical_device, vk_surface, &count, NULL);
+		assert(result == VK_SUCCESS);
+
+		VkSurfaceFormatKHR* surface_formats = malloc(sizeof(VkSurfaceFormatKHR) * count);
+		result = vkGetPhysicalDeviceSurfaceFormatsKHR(vk_physical_device, vk_surface, &count, surface_formats);
+		assert(result == VK_SUCCESS);
+
+		for (uint32_t i = 0; i < count; ++i)
+		{
+			if (surface_formats[i].format == VK_FORMAT_B8G8R8A8_SRGB)
+			{
+				// this is the ideal
+				surface_format = surface_formats[i];
+				break;
+			}
+			else if (surface_formats[i].format == VK_FORMAT_R8G8B8A8_SRGB)
+			{
+				// this is the backup
+				surface_format = surface_formats[i];
+			}
+			// todo: do we need to support unorm formats if neither of these are supported?
+		}
+		assert(surface_format.format);
+
+		VkPresentModeKHR present_mode = VK_PRESENT_MODE_FIFO_KHR;
+		{
+			result = vkGetPhysicalDeviceSurfacePresentModesKHR(vk_physical_device, vk_surface, &count, NULL);
+			assert(result == VK_SUCCESS);
+
+			VkPresentModeKHR* present_modes = malloc(sizeof(VkPresentModeKHR) * count);
+			result = vkGetPhysicalDeviceSurfacePresentModesKHR(vk_physical_device, vk_surface, &count, present_modes);
+			assert(result == VK_SUCCESS);
+
+			for (uint32_t i = 0; i < count; ++i)
+			{
+				if (present_modes[i] == VK_PRESENT_MODE_MAILBOX_KHR)
+				{
+					present_mode = VK_PRESENT_MODE_MAILBOX_KHR;
+					break;
+				}
+			}
+			// todo should be driven by settings, vsync on = FIFO, vsync off = Mailbox if available otherwise immediate
+		}
+
+		VkSwapchainCreateInfoKHR create_info = {
+			.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
+			.surface = vk_surface,
+			.minImageCount = surface_capabilities.minImageCount + 1,
+			.imageFormat = surface_format.format,
+			.imageColorSpace = surface_format.colorSpace,
+			.imageExtent = surface_capabilities.currentExtent,
+			.imageArrayLayers = 1,
+			.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+			.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE, // we're assuming that our graphics queue will be able to present
+			.queueFamilyIndexCount = queue_family_index_count,
+			.pQueueFamilyIndices = queue_family_indices,
+			.preTransform = surface_capabilities.currentTransform,
+			.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,
+			.presentMode = present_mode,
+			.clipped = VK_TRUE,
+			.oldSwapchain = NULL // this should be non-null if resizing the swapchain
+		};
+		result = vkCreateSwapchainKHR(vk_device, &create_info, NULL, &vk_swapchain);
+		assert(result == VK_SUCCESS);
+	}
+
+	{
+		uint32_t count;
+		result = vkGetSwapchainImagesKHR(vk_device, vk_swapchain, &count, NULL);
+		assert(result == VK_SUCCESS);
+
+		VkImage* images = malloc(sizeof(VkImage) * count);
+		result = vkGetSwapchainImagesKHR(vk_device, vk_swapchain, &count, images);
+		assert(result == VK_SUCCESS);
+
+		VkImageView* image_views = malloc(sizeof(VkImageView) * count);
+		for (uint32_t i = 0; i < count; ++i)
+		{
+			typedef struct VkImageViewCreateInfo {
+				VkStructureType            sType;
+				const void* pNext;
+				VkImageViewCreateFlags     flags;
+				VkImage                    image;
+				VkImageViewType            viewType;
+				VkFormat                   format;
+				VkComponentMapping         components;
+				VkImageSubresourceRange    subresourceRange;
+			} VkImageViewCreateInfo;
+			VkImageViewCreateInfo create_info = {
+				.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+				.image = images[i],
+				.viewType = VK_IMAGE_VIEW_TYPE_2D,
+				.format = surface_format.format,
+				.components = {
+					.r = VK_COMPONENT_SWIZZLE_IDENTITY,
+					.g = VK_COMPONENT_SWIZZLE_IDENTITY,
+					.b = VK_COMPONENT_SWIZZLE_IDENTITY,
+					.a = VK_COMPONENT_SWIZZLE_IDENTITY
+				},
+				.subresourceRange = {
+					.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+					.baseArrayLayer = 0,
+					.layerCount = 1,
+					.baseMipLevel = 0,
+					.levelCount = 1
+				}
+			};
+			result = vkCreateImageView(vk_device, &create_info, NULL, &image_views[i]);
+			assert(result == VK_SUCCESS);
+		}
+	}
+
+	{
+		typedef struct VkImageCreateInfo {
+			VkStructureType          sType;
+			const void* pNext;
+			VkImageCreateFlags       flags;
+			VkImageType              imageType;
+			VkFormat                 format;
+			VkExtent3D               extent;
+			uint32_t                 mipLevels;
+			uint32_t                 arrayLayers;
+			VkSampleCountFlagBits    samples;
+			VkImageTiling            tiling;
+			VkImageUsageFlags        usage;
+			VkSharingMode            sharingMode;
+			uint32_t                 queueFamilyIndexCount;
+			const uint32_t* pQueueFamilyIndices;
+			VkImageLayout            initialLayout;
+		} VkImageCreateInfo;
+
+		VkImageCreateInfo create_info = {
+			.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
+			.imageType = VK_IMAGE_TYPE_2D,
+			.format = surface_format.format,
+			.extent = {
+				.width = surface_capabilities.currentExtent.width,
+				.height = surface_capabilities.currentExtent.height,
+				.depth = 1,
+			},
+			.mipLevels = 1,
+			.arrayLayers = 1,
+			.samples = VK_SAMPLE_COUNT_1_BIT,
+			.tiling = VK_IMAGE_TILING_OPTIMAL,
+			.usage = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT,
+			.sharingMode = VK_SHARING_MODE_EXCLUSIVE,
+			.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED
+			// note - no queue family indices needed with an exclusive image
+		};
+		VkImage depth_buffer;
+		result = vkCreateImage(vk_device, &create_info, NULL, &depth_buffer);
+		assert(result == VK_SUCCESS);
+
+		VkMemoryRequirements memory_requirements = { 0 };
+		vkGetImageMemoryRequirements(vk_device, depth_buffer, &memory_requirements);
+
+		uint32_t memory_type_index = -1;
+		VkPhysicalDeviceMemoryProperties memory_properties = { 0 };
+		vkGetPhysicalDeviceMemoryProperties(vk_physical_device, &memory_properties);
+		for (uint32_t i = 0; i < memory_properties.memoryTypeCount; ++i)
+		{
+			if (memory_properties.memoryTypes[i].propertyFlags & VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT &&
+				memory_requirements.memoryTypeBits & (1 << i))
+			{
+				memory_type_index = i;
+				break;
+			}
+		}
+		assert(memory_type_index != -1);
+
+		VkMemoryAllocateInfo alloc_info = {
+			.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
+			.allocationSize = memory_requirements.size,
+			.memoryTypeIndex = memory_type_index
+		};
+		VkDeviceMemory memory;
+		result = vkAllocateMemory(vk_device, &alloc_info, NULL, &memory);
+		assert(result == VK_SUCCESS);
+
+		result = vkBindImageMemory(vk_device, depth_buffer, memory, 0);
+		assert(result == VK_SUCCESS);
+	}
 
 	uint16_t file_path[MAX_PATH];
 	int32_t cmd_line_len = lstrlenW(cmd_line);
